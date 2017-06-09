bed8192cca0bba63c303212f5ba34cd3b8b47626
Thirasan <thirasan@Thirasans-MacBook-Pro.local>
2017-06-09 12:45:11 +0700

    Start_Project

---
+++ app/src/main/java/com/example/thirasan/patient_queue/Presenter.java
@@ -0,0 +1,14 @@
+package com.example.thirasan.patient_queue;
+
+/**
+ * Created by thirasan on 6/9/2017 AD.
+ */
+
+public class Presenter {
+
+    AppView view;
+
+    public Presenter(AppView view){
+        this.view = view;
+    }
+}
